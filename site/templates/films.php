c66bc1ec8a25f760bcaf931fb727874605dd81a0
bnomei <b@bnomei.com>
2024-06-14 19:43:06 +0200

    :construction: films with filter by feature
    Signed-off-by: bnomei <b@bnomei.com>

--- site/templates/films.php
+++ site/templates/films.php
@@ -1,8 +1,29 @@
-<?php $objectsCount = 0; ?>
+<?php $objectsCount = 0;
+
+$films = $page->children();
+if ($feature = get('feature')) {
+  $films = $films->filterBy('special_features', $feature, ',');
+  ?>
+  <nav>
+    <?php $features = [];
+    foreach ($films as $film) {
+      $features = array_merge($features, explode(',', $film->special_features()->value()));
+      sort($features);
+    } ?>
+    <ul>
+      <?php foreach (array_unique($features) as $feature) {
+        $objectsCount++;
+        ?>
+        <li><a href="<?= $page->url() . '?feature=' . $feature ?>"><?= $feature ?></a></li>
+      <?php } ?>
+    </ul>
+  </nav>
+<?php } ?>
+
 <ol>
   <?php
   /** @var \Kirby\Cms\Page $page * */
-  foreach ($page->children() as $film) {
+  foreach ($films as $film) {
       $objectsCount++;
       ?>
     <li>
