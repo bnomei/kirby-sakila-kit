be44eab4e06fafae14bfe6ca7c80c6815518c38a
bnomei <b@bnomei.com>
2024-06-15 12:21:41 +0200

    :construction: resolving merge with main
    Signed-off-by: bnomei <b@bnomei.com>

--- site/templates/films.php
+++ site/templates/films.php
@@ -1,44 +1,54 @@
-<?php
-$objectsCount = 0;
-$films = $page->children();
+<?php snippet('layouts/default', slots: true); ?>
 
-if ($feature = get('feature')) { ?>
+<blockquote>
+  <ul>
+    <li><a href="/film">Load all films</a></li>
+    <li><a href="/film?actors=1">Load all films and resolve actors for each film</a></li>
+    <li><a href="/film?feature=Trailers">Load all films, extract all features as filter and apply one</a></li>
+  </ul>
+</blockquote>
+
+<?php $modelCount = 0;
+
+$films = $page->children();
+if ($feature = get('feature')) {
+    ?>
   <nav>
     <?php
-    // get all filterable features
-    $features = lapse([/*$films,*/ 'features'], function () use ($films) {
-      $features = [];
-      foreach ($films as $film) {
-        $features = array_values(array_merge($features, explode(',', $film->special_features()->value())));
+      // get all filterable features
+      $features = lapse([/*$films,*/ 'features'], function () use ($films) {
+          $features = [];
+          foreach ($films as $film) {
+              $features = array_values(array_merge($features, explode(',', $film->special_features()->value())));
 
-      }
-      $features = array_unique($features);
-      sort($features);
+          }
+          $features = array_unique($features);
+          sort($features);
 
-      return $features;
-    }, 15); ?>
-    <ul>
+          return $features;
+      }, 15); ?>
+    <ul class="filter">
       <?php foreach ($features as $feature) {
-        $objectsCount++;
-        ?>
-        <li><a href="<?= $page->url() . '?feature=' . $feature ?>"><?= $feature ?></a></li>
+          $modelCount++;
+          ?>
+        <li><a href="<?= $page->url().'?feature='.$feature ?>"><?= $feature ?></a></li>
       <?php } ?>
     </ul>
   </nav>
   <?php
   // then filter by feature
   $filmIds = lapse([/*$films,*/ $feature], function () use ($films, $feature) {
-    return $films->filterBy('special_features', $feature, ',')->map(fn($page) => $page->uuid()->toString())->values();
+      return $films->filterBy('special_features', $feature, ',')->map(fn ($page) => $page->uuid()->toString())->values();
   }, 15);
 
-  $films = new \Kirby\Cms\Pages($filmIds);
+    $films = new \Kirby\Cms\Pages($filmIds);
 } ?>
 
 <ol>
   <?php
   /** @var \Kirby\Cms\Page $page * */
   foreach ($films as $film) {
-      $objectsCount++;
+      $modelCount++;
       ?>
     <li>
     <a href="<?= $film->url() ?>"><?= $film->title() ?></a><br>
@@ -47,7 +57,7 @@ if ($feature = get('feature')) { ?>
         <summary>Actors</summary>
         <ul>
           <?php foreach ($film->actors()->toPages() as $actor) {
-              $objectsCount++;
+              $modelCount++;
               ?>
             <li><?= $actor->title() ?></li><?php
           } ?>
@@ -57,4 +67,4 @@ if ($feature = get('feature')) { ?>
   <?php } ?>
 </ol>
 
-<h2><?= $objectsCount ?> objects</h2>
+<div id="modelCount"><?= $modelCount ?></div>
